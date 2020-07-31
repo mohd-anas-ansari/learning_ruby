584acbbb2bca27b6e57d8910ca41ca4cf431474a
anasansari157 <anasansari157@gmail.com>
2020-07-31 13:04:20 +0530

    Add variables

---
+++ variables.rb
@@ -0,0 +1,11 @@
+name = "Anas"
+age= 35
+language_used = "Ruby"
+
+puts "My name is #{name} and I'm #{age}"
+puts "I'm a " + language_used + " Developer" + age.to_s
+
+language_used = "JavaScript"
+
+puts "I'm also a #{language_used} Developer"
+
